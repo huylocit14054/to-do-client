f07bb156f8279c2faa23c0ce78939fa7ebd34b46
Loc Le <sasuke1996vn@gmail.com>
2020-06-23 14:08:21 +0700

    (LocLe) init react app with eslint and scss

---
+++ src/components/App/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import logo from 'assets/icons/logo.svg';
+import css from './App.module.scss';
+
+function App() {
+  return (
+    <div className={css['App']}>
+      <header className={css['App-header']}>
+        <img src={logo} className={css['App-logo']} alt="logo" />
+        <p>
+          Edit <code>src/App.js</code>
+          and save to reload.
+        </p>
+        <a
+          className={css['App-link']}
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
